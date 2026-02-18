556bc3c0919103f57f73cdaef1f379a7ce592f42
Sacar <sakarstha42@gmail.com>
2026-02-18 10:28:09 +0545

    handle auto suggest dropdowns

--- tests/AutoSuggestDropDown.spec.js
+++ tests/AutoSuggestDropDown.spec.js
@@ -1,7 +1,24 @@
 const { test, expect } = require("@playwright/test");
 
 test("Auto Suggest Dropdown", async ({ page }) => {
-  await page.goto("https://www.redbus.in/");
+  await page.goto("https://www.daraz.com.np/");
+
+  await page.locator("#q").fill("Nike");
+  await page.waitForSelector("//div[@class='suggest-list--3Tm8']//a/div/span");
+
+  const searchOptions = await page.$$(
+    "//div[@class='suggest-list--3Tm8']//a/div/span",
+  );
+  // console.log("options are", searchOptions);
+
+  for (let option of searchOptions) {
+    const value = await option.textContent();
+    // console.log("value is", value);
+    if (value.includes("Nike p6000")) {
+      await option.click();
+      break;
+    }
+  }
 
   await page.waitForTimeout(5000);
 });

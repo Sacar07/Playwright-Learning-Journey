defab525755f14cf2b629e55292d59d05a8bcc05
Sacar <sakarstha42@gmail.com>
2026-02-19 10:45:53 +0545

    handle nested frames/iFrames

---
+++ tests/NestedFrames.spec.js
@@ -0,0 +1,17 @@
+const { test, expect } = require("@playwright/test");
+
+test("Frames", async ({ page }) => {
+  await page.goto("https://ui.vision/demo/webtest/frames/");
+
+  const frame3 = await page.frame({
+    url: "https://ui.vision/demo/webtest/frames/frame_3.html",
+  });
+
+  frame3.locator("input[name='mytext3']").fill("Welcome world!");
+
+  //Nested Frame
+  const childFrame = await frame3.childFrames();
+  await childFrame[0].locator("//*[@id='i6']/div[3]/div").check();
+
+  await page.waitForTimeout(5000);
+});

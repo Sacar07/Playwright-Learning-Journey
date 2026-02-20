9bb1058b7bf1b07aa53bb10b3756f4921a9c059e
Sacar <sakarstha42@gmail.com>
2026-02-20 11:39:46 +0545

    perform mouse drag and drop action

---
+++ tests/DragAndDrop.spec.js
@@ -0,0 +1,20 @@
+const { test, expect } = require("@playwright/test");
+
+test("Mouse Double Click", async ({ page }) => {
+  await page.goto("https://testautomationpractice.blogspot.com/");
+
+  const source = page.locator("#draggable");
+  const target = page.locator("#droppable");
+
+  //approach 1
+  /*  await source.hover();
+  await page.mouse.down();
+
+  await target.hover();
+  await page.mouse.up(); */
+
+  //approach 2
+  await source.dragTo(target);
+
+  await page.waitForTimeout(5000);
+});
